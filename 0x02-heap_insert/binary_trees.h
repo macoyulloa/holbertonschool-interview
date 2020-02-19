e356feef0a9ba6b656833857c0d49669f0402657
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2020-02-19 04:29:29 +0000

    binary tree

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -25,39 +25,11 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 /*Basic Binary Tree*/
-typedef struct binary_tree_s bst_t;
-
-/*Basic Binary Tree*/
-typedef struct binary_tree_s avl_t;
-
-/*Basic Binary Tree*/
 typedef struct binary_tree_s heap_t;
-
-
-/* Auxiliar functions */
-
-void replace_left(binary_tree_t *parent, binary_tree_t *node);
 
 /*Mandatory Task Functions*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-size_t binary_tree_height(const binary_tree_t *tree);
-size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
-size_t binary_tree_leaves(const binary_tree_t *tree);
-size_t binary_tree_nodes(const binary_tree_t *tree);
-int binary_tree_balance(const binary_tree_t *tree);
-int binary_tree_is_full(const binary_tree_t *tree);
-int binary_tree_is_perfect(const binary_tree_t *tree);
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /*Functions printing*/
 void binary_tree_print(const binary_tree_t *);
