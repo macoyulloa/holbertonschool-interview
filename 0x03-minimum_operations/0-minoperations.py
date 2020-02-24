3908bfb3c332ade325676964fcc50f276ff861e6
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2020-02-24 18:31:33 +0000

    interviwe points

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""calculates the fewest number of operations needed"""
+
+
+def isPrime(num):
+    """check if the number is prime"""
+    if num <= 2:
+        return True
+    value = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            value += 1
+            if value > 2:
+                return False
+    return True
+
+
+def primeDescompose(x):
+    """descompose x into a primes"""
+    val = x
+    primes = []
+    for i in range(2, x + 1):
+        if isPrime(i):
+            if x % i == 0:
+                while x % i == 0:
+                    x = x / i
+                    primes.append(i)
+            else:
+                pass
+    return primes
+
+
+def minOperations(n):
+    """minimum of operation"""
+    if n <= 0:
+        return 0
+    if isPrime(n):
+        return n
+    list_primes = primeDescompose(n)
+    return sum(list_primes)
