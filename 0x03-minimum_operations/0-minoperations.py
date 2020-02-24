81491c22d131b0280f5aa689528f01c2df002ae0
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2020-02-24 19:48:13 +0000

    special cases

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,9 +7,12 @@ def minOperations(n):
     if n < 2 or type(n) is not int:
         return 0
     descompose = []
-    for i in range(2, n + 1):
-        if n % i == 0:
-            while n % i == 0 and n != 1:
-                n = n / i
+    val = n
+    i = 1
+    while val != 1:
+        i += 1
+        if val % i == 0:
+            while (val % i == 0 and val != 1):
+                val = val / i
                 descompose.append(i)
     return sum(descompose)
