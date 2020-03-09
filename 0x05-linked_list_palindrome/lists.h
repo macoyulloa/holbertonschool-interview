d24ba8af50b7a3733156bcf2ca72879b78fcb5ed
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-09 18:01:22 +0000

    correction

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -18,8 +18,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
 int is_palindrome(listint_t **head);
-void reverse(listint_t **head);
-int compareLists(listint_t **head, listint_t *middle);
-listint_t *middleList(listint_t **head);
+
 #endif /* LISTS_H */
