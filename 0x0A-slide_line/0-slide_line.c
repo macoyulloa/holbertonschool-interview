3cc655f3022c5bbb84cceded728b1c1326be57a2
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-12 05:43:32 +0000

    betty style corrections

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -2,13 +2,23 @@
 #include <stdio.h>
 #include "slide_line.h"
 
-void swap(int* xp, int* yp)
+/**
+ * swap - swaps the position of two number in an array
+ * @min_n: minimun number
+ * @n: bigger number
+ */
+void swap(int *min_n, int *n)
 {
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
+	int temp = *min_n;
+	*min_n = *n;
+	*n = temp;
 }
 
+/**
+ * sort - sorting an array of ints
+ * @line: pointer to a first number
+ * @size: size of the array
+ */
 void sort(int *line, size_t size)
 {
 	size_t i, j, min_idx;
@@ -21,6 +31,30 @@ void sort(int *line, size_t size)
 				min_idx = j;
 
 		swap(&line[min_idx], &line[i]);
+	}
+}
+
+/**
+ * left - if the case if move to the direction left
+ * @line: pointer to a first number
+ * @size: size of the array
+ */
+void left(int *line, size_t size)
+{
+	size_t i, j;
+
+	if (line[0] == 0)
+	{
+		for (i = 0, j = 0; i < size;)
+		{
+			if (line[i] != 0)
+			{
+				line[j] = line[i];
+				line[i] = 0;
+				j++;
+			}
+			i++;
+		}
 	}
 }
 
@@ -39,7 +73,7 @@ int slide_line(int *line, size_t size, int direction)
 		if (direction != 1)
 			return (0);
 
-	for (i=0, j=0; i < size;)
+	for (i = 0, j = 0; i < size;)
 	{
 		if (line[i] == 0)
 		{
@@ -65,20 +99,7 @@ int slide_line(int *line, size_t size, int direction)
 	sort(line, size);
 
 	if (direction == 0)
-	{
-		if (line[0] == 0)
-		{
-			for (i=0, j=0; i < size;)
-			{
-				if (line[i] != 0)
-				{
-					line[j] = line[i];
-					line[i] = 0;
-					j++;
-				}
-				i++;
-			}
-		}
-	}
+		left(line, size);
+
 	return (1);
 }
