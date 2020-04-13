f6230f68c58159abe2a82253460868c4bd0b9e5e
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-13 19:08:46 +0000

    correction of the loop range

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -69,9 +69,8 @@ int slide_line(int *line, size_t size, int direction)
 {
 	size_t i, j;
 
-	if (direction != 0)
-		if (direction != 1)
-			return (0);
+	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
+		return (0);
 
 	for (i = 0, j = 0; i < size;)
 	{
@@ -80,6 +79,8 @@ int slide_line(int *line, size_t size, int direction)
 			while (line[i] == 0)
 				i++;
 		}
+		if (i == (size - 1))
+			break;
 		j = i + 1;
 		if (line[j] == 0)
 		{
@@ -94,8 +95,8 @@ int slide_line(int *line, size_t size, int direction)
 		}
 		else
 			i++;
+		j = 0;
 	}
-
 	sort(line, size);
 
 	if (direction == 0)
