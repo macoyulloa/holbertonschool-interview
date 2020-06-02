10c48cbc92c527e026ec6fbddcfc88a9dee768b9
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-06-02 17:51:03 +0000

    get the characteres of the star wars api

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,17 +2,24 @@
 
 const request = require("request");
 
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
-
-request(url, async function (error, response, body) {
-  const characters = JSON.parse(body).characters;
-  for (const character of characters) {
-    const res = await new Promise((resolve, reject) => {
-      request(character, (err, res, html) => {
-        resolve(JSON.parse(html).name);
+try {
+  const movieId = process.argv[2];
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  request(url, async function (error, response, body) {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
       });
-    });
-    console.log(res);
-  }
-});
+      console.log(res);
+    }
+  });
+} catch (catchErr) {
+  console.log(catchErr);
+}
